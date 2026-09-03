27dacecb71c42f439c1425b4507f10b5a8dd622d
Ayesha Majeed <ayesha@example.com>
2026-09-03 20:17:58 +0500

    Clean up unused old files and duplicates

--- DesktopApp/test_ui.py
+++
@@ -1,8 +0,0 @@
-import sys
-try:
-    from frontend.sync_app_ui import SyncAppUI
-    app = SyncAppUI(None, {"id": "test"}, None)
-    app._show_detail("748", doc=None)
-except Exception as e:
-    import traceback
-    traceback.print_exc()
